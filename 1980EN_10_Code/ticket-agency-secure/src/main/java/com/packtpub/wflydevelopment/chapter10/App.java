884779e812ded8504b0f89be56e4432004fbf6c5
emag <lanabe.lanabe@gmail.com>
2015-12-02 02:52:23 +0900

    Done 287/400

--- 1980EN_10_Code/ticket-agency-secure/src/main/java/com/packtpub/wflydevelopment/chapter10/App.java
+++ 1980EN_10_Code/ticket-agency-secure/src/main/java/com/packtpub/wflydevelopment/chapter10/App.java
@@ -3,7 +3,7 @@ package com.packtpub.wflydevelopment.chapter10;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.container.Container;
-import org.wildfly.swarm.security.SecurityFraction;
+import org.wildfly.swarm.management.ManagementFraction;
 import org.wildfly.swarm.undertow.WARArchive;
 
 /**
@@ -14,6 +14,16 @@ public class App {
   public static void main(String[] args) throws Exception {
     Container container = new Container();
 
+    container.fraction(ManagementFraction.createDefaultFraction()
+      .securityRealm("ApplicationRealm", (realm) -> {
+        realm.inMemoryAuthentication((authn) -> {
+          authn.add("demouser", "password", true);
+        });
+        realm.inMemoryAuthorization((authz) -> {
+          authz.add("demouser", "Manager");
+        });
+      }));
+
     WARArchive deployment = ShrinkWrap.create(WARArchive.class);
     deployment.addPackages(true, App.class.getPackage());
 
@@ -23,7 +33,7 @@ public class App {
       new ClassLoaderAsset("resources/style.css", App.class.getClassLoader()), "resources/style.css");
 
     deployment.addAsWebInfResource(
-      new ClassLoaderAsset("WEB-INF/web.xml", App.class.getClassLoader()), "WEB-INF/web.xml");
+      new ClassLoaderAsset("WEB-INF/web.xml", App.class.getClassLoader()), "web.xml");
     deployment.addAsWebInfResource(
       new ClassLoaderAsset("WEB-INF/templates/default.xhtml", App.class.getClassLoader()), "templates/default.xhtml");
     deployment.addAsWebInfResource(
